cf3f710f916fd15c9be7d6cd86f1ed302324d46e
Andrew Sutton <ajsutton94@gmail.com>
2020-07-04 13:35:52 -0500

    added comments, made changes to accept special symbols at the end of the string

--- MichaelChallenge1/Program.cs
+++ MichaelChallenge1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace MichaelChallenge1
@@ -26,20 +27,27 @@ namespace MichaelChallenge1
         {
             string toReturn = "";
             string tempWord;
+            // loop for going through the entire word
             for (int i = 0; i < str.Length; i++)
             {
+                //Resetting the temporary word
                 tempWord = "";
+                //we are trying to find either a special symbol or the end of the entire string. If either of these conditions are met, the while will be false and break out.
                 while (Char.IsLetter(str[i]) && i != str.Length - 1)
                 {
+                    //just collecting the word until the symbol or the end of the string hits
                     tempWord += str[i];
                     i++;
                 }
-                if (i == str.Length - 1)
+                // this only takes place when we hit the end of the string. it was leaving off the last letter, because it was treating it like a special symbol
+                if (i == str.Length - 1 && Char.IsLetter(str[i]))
                 {
                     tempWord += str[i];
                 } 
+                // reversing the word that was found. This does not contain the special symbol
                 tempWord = ReverseString(tempWord);
-                if (i != str.Length - 1)
+                // tacking on the special symbol, this won't happen at the end of the string if all we have is letters. 
+                if (!Char.IsLetter(str[i]))
                 {
                     tempWord += str[i];
                 }
